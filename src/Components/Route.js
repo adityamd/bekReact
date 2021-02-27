8a9e7aeb6bfafc9778c787eb8f2f1e4762822299
Aditya Tripathi <aditya.t2210@gmail.com>
2021-02-27 20:50:03 +0530

    Fixing Cookies issue

--- src/Components/Route.js
+++ src/Components/Route.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import {Switch, Route, Redirect, Router} from "react-router-dom";
+import {Switch, Route, Redirect, BrowserRouter as Router} from "react-router-dom";
 import Cookies from "universal-cookie";
 
 import Publisher from './Publisher'
