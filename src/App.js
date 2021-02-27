bf9fda0b5a9a80241678f7f581114e002ca08a49
Aditya Tripathi <aditya.t2210@gmail.com>
2021-02-27 20:24:15 +0530

    Fixing Cookies issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import Routing from './Components/Route'
-import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <Routing />
      </div>
-     </Router>
   );
 }
 
